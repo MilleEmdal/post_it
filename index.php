e7da6f3da91040dbd063322a9cbf38221ca24870
nocker27 <lassenn92@gmail.com>
2017-04-05 11:39:03 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,37 +3,35 @@
 <head>
 <meta charset="utf-8">
 <title>Post It</title>
-<link href="style/style.css">
+<link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
 
 <body>
 
-<header>
-	<button><a href="#">Login</a></button>
-</header>
-
 	<div class="menu">
-	
 	<div class="logo">
-		
-	</div>
-		
-	<nav>
-		<ul>
-			<li><a href="index.php">Skriv dit opslag</a></li>
-			<li><a href="#">Se alle poslagene</a></li>
-		</ul>
-	</nav>
+		<img src="post it.png">
+	</div>	
+		<nav>
+			<ul>
+				<br>
+				<li><a href="index.php">Skriv dit opslag</a></li>
+				<li><a href="#">Se alle opslagene</a></li>
+			</ul>
+		</nav>
 	</div>
 	
-
+<div class="fill">
+	
+</div>
 <fieldset>
+	<h2>Opret et Post It</h2>
 	<form>
-		<input type="text" name="name" placeholder="name"><br>
-		<input type="date" name="call" placeholder="last call"><br>
-		<input type="date" name="date" placeholder="date of event"><br>
-		<input type="submit" name="cmd" value="Tilmeld">
-		<input type="submit" name="cmd" value="Afmeld">
+		<input type="text" name="name" placeholder="Name"><br><br>
+		<input type="date" name="call"> Last call<br><br>
+		<input type="date" name="date"> Date of event<br><br>
+		<textarea name="note" placeholder="your note here"></textarea><br><br>
+		<input type="submit" name="cmd" value="Send Opslag">
 	</form>
 	
 </fieldset>
