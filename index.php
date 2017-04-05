53e8a5efa49a6c03e5ecbfa44da3e10960a801fc
MilleEmdal <millememdal@gmail.com>
2017-04-05 11:13:25 +0200

    step1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Post It</title>
+<link href="style/style.css">
+</head>
+
+<body>
+
+<header>
+	<button><a href="#">Login</a></button>
+</header>
+
+	<div class="menu">
+	
+	<div class="logo">
+		
+	</div>
+		
+	<nav>
+		<ul>
+			<li><a href="index.php">Skriv dit opslag</a></li>
+			<li><a href="#">Se alle poslagene</a></li>
+		</ul>
+	</nav>
+	</div>
+	
+
+<fieldset>
+	<form>
+		<input type="text" name="name" placeholder="name"><br>
+		<input type="date" name="call" placeholder="last call"><br>
+		<input type="date" name="date" placeholder="date of event"><br>
+		<input type="submit" name="cmd" value="Tilmeld">
+		<input type="submit" name="cmd" value="Afmeld">
+	</form>
+	
+</fieldset>
+
+</body>
+</html>
